23e74182f1abc81207bdf06b84d4b6d7951517fb
LeMiBad <79872121399.yandex@gmail.com>
2024-05-30 12:13:54 +0300

    fix

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import routes from "./src/routes/routes";
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Подключаем маршруты
+app.use("/api", routes);
+
+// Обработчик ошибок 404 должен быть после маршрутов
+// app.use(errorHandlerMiddleware);
+
+app.listen(80, () => console.log("Server is running on port 80"));
+
+export default app;
