5a312a73be24f0826342547dc2b40f74fa939af2
LeMiBad <79872121399.yandex@gmail.com>
2024-06-01 17:25:21 +0300

    fix

--- src/services/cohere/callFunctions/setTarget.ts
+++ src/services/cohere/callFunctions/setTarget.ts
@@ -1,5 +1,6 @@
 import { Chats } from "@prisma/client";
 import prisma from "../../../config/prisma";
+import { sendToBot } from "../../../utils/sendToBot";
 
 interface Info {
   targets: {
@@ -9,6 +10,13 @@ interface Info {
 }
 
 export const setTarget = async (chat: Chats, info: Info) => {
+  //@ts-expect-error
+  const userName = chat?.user_info?.find(info => info?.type === 'name').value ?? chat.id
+
+  const targetsString = info.targets.map(target => `${target.type}: ${target.value}`)
+
+  await sendToBot(`Пользователь ${userName} совершил целевое действие \n ${targetsString.join('\n\n')}`)
+
   await prisma.chats.update({
     where: {
       id: chat.id,
--- src/services/cohere/callFunctions/setUserInfo.ts
+++ src/services/cohere/callFunctions/setUserInfo.ts
@@ -1,5 +1,6 @@
 import { Chats } from "@prisma/client";
 import prisma from "../../../config/prisma";
+import { sendToBot } from "../../../utils/sendToBot";
 
 interface Info {
   user_info: {
@@ -9,6 +10,14 @@ interface Info {
 }
 
 export const setUserInfo = async (chat: Chats, info: Info) => {
+
+  //@ts-expect-error
+  const userName = chat?.user_info?.find(info => info?.type === 'name').value ?? chat.id
+
+  const infosString = info.user_info.map(target => `${target.type}: ${target.value}`)
+
+  await sendToBot(`Пользователь ${userName} поделился информацией \n ${infosString.join('\n\n')}`)
+
   await prisma.chats.update({
     where: {
       id: chat.id,
