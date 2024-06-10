cc1e24eec5f36b9916189f65284a04c44bfa19a3
LeMiBad <79872121399.yandex@gmail.com>
2024-06-10 19:14:10 +0300

    fix

--- index.ts
+++ index.ts
@@ -10,12 +10,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Подключаем маршруты
 app.use("/api", routes);
 
-// Обработчик ошибок 404 должен быть после маршрутов
-// app.use(errorHandlerMiddleware);
-
-app.listen(80, () => console.log("Server is running on port 80"));
+app.listen(80, () => console.log("Сервер запущен на порту 80"));
 
 export default app;
